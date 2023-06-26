ee546bb8fe4e2d1dcf94ac48692e1511fcd25e41
Griffins2005 <134501193+Griffins2005@users.noreply.github.com>
2023-06-26 20:06:31 +0400

    first commit

---
+++ viteReact/src/App.jsx
@@ -0,0 +1,17 @@
+import React,{ useState } from 'react';
+import Header from './components/Header';
+import Meme from './components/Meme';
+
+function App () {
+
+  return (
+    <>
+      <div>
+        <Header />
+        <Meme />
+      </div>
+    </>
+  )
+}
+
+export default App
